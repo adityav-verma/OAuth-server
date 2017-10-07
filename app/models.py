27c5bb6888496ed398879bc08e55df933bc44345
coderadi <meetadi3@gmail.com>
2017-10-08 03:29:56 +0530

    Add check_password instance method to user class

--- app/models.py
+++ app/models.py
@@ -2,8 +2,11 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     password = db.Column(db.String(255), nullable=False)
+
+    def check_password(self, password):
+        return self.password == password
 
     def __repr__(self):
         return '<User id: {}, name: {}>'.format(self.id, self.name)
