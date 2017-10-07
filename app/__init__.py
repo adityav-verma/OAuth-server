037e8dbe3dafae923a859edb742c5eed21538f92
coderadi <meetadi3@gmail.com>
2017-10-07 22:15:08 +0530

    Add Flask-Migrate
    Flaks-Migrate will maintain db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///oauth.db'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
--- manage.py
+++ manage.py
@@ -1,10 +1,12 @@
 from flask.ext.script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
 
-from app import app
+migrate = Migrate(app, db)
 
 manager = Manager(app)
-
 manager.add_command('runserver', Server())
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
